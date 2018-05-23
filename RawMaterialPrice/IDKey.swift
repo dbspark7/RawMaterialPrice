a6c74fde4e749abea10a7363643e5af00153b6ba
Soo Seong Park <dbspark7@icloud.com>
2018-05-23 11:03:17 +0900

    1.0.2

---
+++ RawMaterialPrice/IDKey.swift
@@ -0,0 +1,23 @@
+//
+//  IDKey.swift
+//  RawMaterialPrice
+//
+//  Created by 박수성 on 2018. 5. 23..
+//  Copyright © 2018년 dbspark7. All rights reserved.
+//
+
+import Foundation
+
+struct IDKey {
+    // Google Mobile Ads SDK ID
+    static let ADS_SDK_ID = "secret"
+    
+    // Ad Unit ID
+    static let AD_UNIT_ID = "secret"
+    
+    // Quandl API Key
+    static let QUANDL_API_KEY = "secret"
+    
+    // Koreaexim API Key
+    static let KOREAEXIM_API_KEY = "secret"
+}
